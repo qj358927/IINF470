41be003db5ddbd7bc42d083d0cd18a382dbd2f4c
qj358927 <qj358927@gmail.com>
2014-04-17 12:39:46 -0400

    Create Quadcopter.ino
    Basic skeleton for our code

---
+++ Quadcopter.ino
@@ -0,0 +1,56 @@
+/*RAT Group 2
+ *
+ *Members:
+ * --David Vadney
+ * --Dan Van Wormer
+ * --Nick Doran
+ * --Dong Luu
+ * --Brendan Farr
+ * --Rebecca Suafoa
+ *
+ *
+ *Purpose: 
+ *  We will be building our codebase for the quadcopter flying and stabilizing logic in this file.
+ *  Compiled using arduino sketch.
+ */
+ 
+ 
+ /*Define Pins for interfacing with the ESCs*/
+ 
+ 
+ /*End ESCs*/
+ 
+ 
+ /*Define Pins for interfacing with the RC remote*/
+ 
+ /*End Remote*/
+ 
+ /*Our Setup function should be a quick testing and diagnositics
+ * section to prevent ruining the copter during execution*/
+ 
+ setup(){
+ 
+ }
+ 
+ 
+ /*loop should deal with repeatedly polling the remote
+  *and relaying that information to the ESCs for stearing and control*/
+  
+  loop(){
+  
+  }
+ 
+ /*Functions for the ESCs*/
+ 
+ 
+ 
+ 
+ /*Functions for the Remote*/
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
